39c35f4765aebffec0e7c769287f7c8c9d3c5b97
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-03-11 19:09:15 -0300

    Concluido conversor

---
+++ conversorMoedas/script.js
@@ -0,0 +1,16 @@
+function Converter() {
+
+    var valueElem = document.getElementById("valor")
+    var value = valueElem.value
+    var valueDolar = parseFloat(value)
+
+
+
+    var valueInReal = valueDolar * 5
+    console.log(valueInReal)
+
+    var valueConverted = document.getElementById("valorConvertido")
+    var result = "O resultado em Real é  R$ " + valueInReal
+    valueConverted.innerHTML = result
+
+}
